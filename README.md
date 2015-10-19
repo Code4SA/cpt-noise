8a02edc3bb2c6ceb936e2273542a3be4d76f290c
Greg Kempe <greg@kempe.net>
2015-10-19 21:13:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Cape Town Airport Runway Noise Impact
+=====================================
+
+Tool to help folks understand the noise impact of Cape Town's International Airport's
+proposed new runway.
+
+Production deployment
+---------------------
+
+Just deploy to your gh-pages branch with git push!
+
+License
+-------
+
+MIT License
---
+++ js/app.js
@@ -0,0 +1,77 @@
+$(function() {
+  var marker;
+  var map = L.map('map', {
+    scrollWheelZoom: false,
+    zoomControl: true,
+  });
+  
+  map.attributionControl.setPrefix('');
+  map.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 18
+  }));
+
+  var newNoise = new L.GeoJSON(newNoiseGeoJson, {
+    style: function(feat) {
+      return {
+        clickable: false,
+        stroke: true,
+        color: '#700',
+        weight: 1,
+        fillOpacity: 0.25,
+        fillColor: '#ED1C24',
+      };
+    }
+  });
+  map.addLayer(newNoise);
+  map.fitBounds(newNoise.getBounds());
+
+
+  function showReport(latlng) {
+    // which feature is it in?
+    var matches = leafletPip.pointInLayer(latlng, newNoise, false);
+
+    if (!matches.length) {
+      $("#report").addClass('hidden');
+      return;
+    }
+
+    var loudest = _.max(matches, function(m) { return m.feature.properties.Noise_Max_; });
+    $('#report')
+      .removeClass('hidden')
+      .find('.decibels')
+        .text(loudest.feature.properties.Noise_Max_);
+  }
+
+
+  function showPoint(latlng) {
+    if (!marker) { 
+      marker = L.marker(latlng).addTo(map);
+    } else {
+      marker.setLatLng(latlng);
+    }
+
+    // how bad is the noise?
+    showReport(latlng);
+  }
+
+
+  map.on('click', function(e) {
+    showPoint(e.latlng);
+  });
+
+
+  $('.find-address').on('click', function(e) {
+    e.preventDefault();
+    var addr = $('#address').val();
+
+    $.getJSON('http://mapit.code4sa.org/address?generation=1&type=CY&partial=1&address=' + encodeURIComponent(addr))
+      .done(function(result) {
+        if (result.addresses.length) {
+          var addr = result.addresses[0];
+
+          showPoint(L.latLng(addr.lat, addr.lng));
+        }
+      });
+  });
+});
